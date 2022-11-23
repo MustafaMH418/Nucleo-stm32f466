17b5f5e69e3286be4ce75b1e24dabb106a8cd925
Mustafa Abd El-Kader <85041890+MustafaMH418@users.noreply.github.com>
2022-11-23 09:20:22 +0200

    Create main.h

---
+++ Clocks/PLL/PLL_MAX_180MHz/main.h
@@ -0,0 +1,16 @@
+
+#ifndef INC_MAIN_H_
+#define INC_MAIN_H_
+
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx.h"
+#include "stm32f446xx.h"
+#include <string.h>
+#include <stdio.h>
+
+#define SYS_CLK_FREQ_50_MHZ				50
+#define SYS_CLK_FREQ_84_MHZ				84
+#define SYS_CLK_FREQ_120_MHZ			120
+#define SYS_CLK_FREQ_180_MHZ			180
+
+#endif /* INC_MAIN_H_ */
